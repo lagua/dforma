ab1f1b8365ed925833b1ff963e875ceeb4ba405d
wshager <wshager@gmail.com>
2013-02-02 17:24:15 +0100

    added message to group

--- Group.js
+++ Group.js
@@ -11,8 +11,9 @@ define([
 	"dijit/_Contained"
 ],function(declare,lang,domStyle,_Widget,_TemplatedMixin,_Container,_Contained){
 return declare("dforma.Group",[_Widget,_TemplatedMixin,_Container,_Contained],{
-	templateString: "<div class=\"dijit dijitReset dformaGroup\" aria-labelledby=\"${id}_label\"><span style=\"display:none\" class=\"dijitReset dijitInline dformaGroupLabel\" data-dojo-attach-point=\"labelNode\" id=\"${id}_label\"></span><span id=\"${id}_containerNode\" data-dojo-attach-point=\"containerNode\"></span></div>",
+	templateString: "<div class=\"dijit dijitReset dformaGroup\" aria-labelledby=\"${id}_label\"><span style=\"display:none\" class=\"dijitReset dijitInline dformaGroupLabel\" data-dojo-attach-point=\"labelNode\" id=\"${id}_label\"></span><span id=\"${id}_messageNode\" data-dojo-attach-point=\"messageNode\"></span><span id=\"${id}_containerNode\" data-dojo-attach-point=\"containerNode\"></span></div>",
 	label: "",
+	message:"",
 	startup:function(){
 		if(this.label) domStyle.set(this.id+"_label","display","inline-block");
 		this.inherited(arguments);
@@ -24,6 +25,14 @@ return declare("dforma.Group",[_Widget,_TemplatedMixin,_Container,_Contained],{
 		//		Set the label (text) of the button; takes an HTML string.
 		this._set("label", content);
 		this["labelNode"].innerHTML = content;
+ 	},
+ 	_setMessageAttr: function(/*String*/ content){
+		// summary:
+		//		Hook for set('label', ...) to work.
+		// description:
+		//		Set the label (text) of the button; takes an HTML string.
+		this._set("message", content);
+		this["messageNode"].innerHTML = content;
  	}
 });
 });
