0cd1358b5eb8eb2e193b8d12f8cf75e530263c0e
W.S. Hager <wshager@gmail.com>
2013-05-27 10:52:52 +0200

    added nls

---
+++ nls/nl/common.js
@@ -0,0 +1,11 @@
+define(
+({
+	buttonOk: "OK",
+	buttonCancel: "Annuleren",
+	buttonSubmit: "Verzenden",
+	buttonSave: "Opslaan",
+	buttonAdd: "Toevoegen",
+	buttonRemove: "Verwijderen",
+	itemClose: "Sluiten"
+})
+);
