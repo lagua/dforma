781eba019c0f51dcb1c9959c14aaf5b62790d8a6
wshager <wshager@gmail.com>
2013-05-28 14:42:42 +0200

    moved jsv to own lib

--- jsonschema.js
+++ jsonschema.js
@@ -1,7 +1,10 @@
-define(["dojo/_base/lang","./jsv","./json-schema-draft-03"],function(lang,jsv){
+define([
+	"dojo/_base/lang",
+	"jsv/lib/jsv",
+	"jsv/lib/json-schema-draft-03"
+],function(lang,_jsv){
 	lang.getObject("dforma", true);
-	var JSV = jsv.JSV;
+	var JSV = _jsv.JSV;
 	dforma.jsonschema = JSV.createEnvironment();
 	return dforma.jsonschema;
 });
-
