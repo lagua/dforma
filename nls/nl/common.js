94dc573cb4a6b6053d929b6d54c5058beef493fc
W.S. Hager <wshager@gmail.com>
2013-05-28 23:38:15 +0200

    added buttons

--- nls/nl/common.js
+++ nls/nl/common.js
@@ -5,6 +5,8 @@ define(
 	buttonSubmit: "Verzenden",
 	buttonSave: "Opslaan",
 	buttonAdd: "Toevoegen",
+	buttonEditSelected: "Bewerk geselecteerde",
+	buttonRemoveSelected: "Verwijder geselecteerde",
 	buttonRemove: "Verwijderen",
 	itemClose: "Sluiten"
 })
