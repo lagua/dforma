485aa962d0ba69d84630037058b8faf489c166a9
wshager <wshager@gmail.com>
2013-06-11 18:38:10 +0200

    unset hidden on domNode, added isHidden method

--- HiddenGroup.js
+++ HiddenGroup.js
@@ -1,15 +1,24 @@
 define([
     	"dojo/_base/declare",
     	"dojo/_base/lang",
+    	"dojo/dom-attr",
     	"dojo/dom-class",
     	"./Group"
-    ],function(declare,lang,domClass,Group){
+    ],function(declare,lang,domAttr,domClass,Group){
 
 	return declare("dforma.HiddenGroup",[Group],{
 		_setLabelAttr:function(){
 		},
+		isHidden:function(){
+			return domClass.contains(this.containerNode,"dijitHidden");
+		},
+		validate:function(){
+			if(this.isHidden()) return true;
+			this.inherited(arguments);
+		},
 		postCreate:function(){
 			this.inherited(arguments);
+			domAttr.set(this.domNode,"hidden",false);
 			domClass.toggle(this.labelNode,"dijitHidden",true);
 			domClass.toggle(this.buttonNode,"dijitHidden",false);
 			domClass.toggle(this.containerNode,"dijitHidden",true);
