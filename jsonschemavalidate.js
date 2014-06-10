c893273abf37cfb9af951640f69e6ccb6834dbbb
whager <whager@benjamins.nl>
2014-06-10 13:36:56 +0200

    commented out dependency

--- jsonschemavalidate.js
+++ jsonschemavalidate.js
@@ -1,10 +1,12 @@
 define([
-	"dojo/_base/lang",
+	"dojo/_base/lang"/*,
 	"jsv/lib/jsv",
-	"jsv/lib/json-schema-draft-03"
-],function(lang,_jsv){
+	"jsv/lib/json-schema-draft-03"*/
+],function(lang/*,_jsv*/){
 	lang.getObject("dforma", true);
-	var JSV = _jsv.JSV;
+	/*var JSV = _jsv.JSV;
 	dforma.jsonschemavalidate = JSV.createEnvironment();
 	return dforma.jsonschemavalidate;
+	*/
+	return true;
 });
