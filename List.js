5fc90d80b1d74811a921a4b363d320fae071deba
W.S. Hager <wshager@gmail.com>
2014-12-14 23:24:47 +0100

    added memory as default store

--- List.js
+++ List.js
@@ -9,13 +9,16 @@ define([
 	"dijit/_Container",
 	"dijit/_TemplatedMixin",
 	"dijit/form/Button",
+	"dstore/Memory",
 	"dgrid/OnDemandGrid",
 	"dgrid/Editor",
 	"dgrid/Keyboard",
 	"dgrid/Selection",
 	"dgrid/extensions/DijitRegistry",
 	"dojox/mobile/i18n"
-],function(declare,lang,array,domConstruct,domClass,_WidgetBase,_Contained,_Container,_TemplatedMixin,Button,OnDemandGrid, Keyboard, Selection, Editor, DijitRegistry,i18n){
+],function(declare,lang,array,domConstruct,domClass,
+		_WidgetBase,_Contained,_Container,_TemplatedMixin, Button, Memory,
+		OnDemandGrid, Keyboard, Selection, Editor, DijitRegistry,i18n){
 	
 	return declare("dforma.List",[_WidgetBase,_Contained,_Container,_TemplatedMixin],{
 		templateString: "<div class=\"dijit dijitReset\" data-dojo-attach-point=\"focusNode\" aria-labelledby=\"${id}_label\"><div class=\"dijitReset dijitHidden dformaListLabel\" data-dojo-attach-point=\"labelNode\" id=\"${id}_label\"></div><div class=\"dijitReset dijitHidden dformaListHint\" data-dojo-attach-point=\"hintNode\"></div><div data-dojo-attach-point=\"containerNode\"></div><div class=\"dijitReset dijitHidden dformaListMessage\" data-dojo-attach-point=\"messageNode\"></div></div>",
@@ -52,6 +55,7 @@ define([
 	 	postCreate:function(){
 			var common = i18n.load("dforma","common");
 			var self = this;
+			if(!this.store) this.store = new Memory();
 			var Widget = declare([OnDemandGrid, Keyboard, Selection, Editor, DijitRegistry],{
 				collection:this.store,
 				selectionMode:"single",
