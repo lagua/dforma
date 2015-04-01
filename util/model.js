bdf9ddedb7e6acdcf973673aa4cdbed1e4fc6546
wshager <wshager@gmail.com>
2015-04-01 12:57:51 +0200

    typo fix

--- util/model.js
+++ util/model.js
@@ -35,7 +35,7 @@ define([
 				var value = data[k]!==undefined ? data[k] : deft ? deft : undefined;
 				if(type) {
 					if (type === 'string') {
-						data[k] = '' + value;
+						value = '' + value;
 					} else if (type === 'number') {
 						value = +value;
 					} else if (type === 'boolean') {
